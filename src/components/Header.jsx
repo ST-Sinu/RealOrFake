2711d53ddea2621912913fe4406dfd49d1e28ad0
ST-Sinu <tlsgusdn98@naver.com>
2025-06-09 16:51:20 +0900

    link change commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,20 @@ function Header() {
     alert("아직 준비중입니다!");
   };
 
+  const handleNow = (e) => {
+    e.preventDefault(); // 링크 이동 막기
+    alert("현재가 1탄입니다!");
+  };
+
   return (
     <header className="header">
       <a href="https://st-sinu.github.io/RealOrFake/">
         <h1>REAL OR FAKE : 진짜를 찾아라</h1>
       </a>
       <div className="series">
-        <a href="https://st-sinu.github.io/RealOrFake/">1탄</a>
+        <a href="https://st-sinu.github.io/RealOrFake/" onClick={handleNow}>
+          1탄
+        </a>
         <a href="/" onClick={handleComingSoon}>
           2탄
         </a>
