cc5c6d2ffaa407213d1ec48d91d9a56e4b762c4f
ST-Sinu <tlsgusdn98@naver.com>
2025-06-10 20:59:36 +0900

    delete visitor counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import VisitorCounter from "./components/VisitorCounter";
 import CoupangAd from "./components/CoupangAd";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         setScore={setScore}
         setCurrentRound={setCurrentRound}
       />
-      <VisitorCounter /> {/* 👈 첫 화면 상단 또는 하단에 표시 */}
       <CoupangAd />
     </div>
   );
