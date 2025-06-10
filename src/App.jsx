4515116f2792b660532a83aecd5c1fd4b8530980
ST-Sinu <tlsgusdn98@naver.com>
2025-06-10 21:26:47 +0900

    Add Samsung Ace AD

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         setCurrentRound={setCurrentRound}
       />
       <CoupangAd />
+      <SamsungBanner />
     </div>
   );
 }
---
+++ src/components/SamsungBanner.jsx
@@ -0,0 +1,18 @@
+// components/SamsungBanner.jsx
+const SamsungBanner = () => {
+  return (
+    <div style={{ textAlign: "center", margin: "1rem auto" }}>
+      <iframe
+        src="https://www.samsung.com/sec/ace/embed/G000319112/KtB/1"
+        width="160"
+        height="320"
+        frameBorder="0"
+        scrolling="no"
+        referrerPolicy="unsafe-url"
+        title="갤럭시 버즈3 프로 배너"
+      ></iframe>
+    </div>
+  );
+};
+
+export default SamsungBanner;
